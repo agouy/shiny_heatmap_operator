f31050fde5a503151aa9a4c5a2bf3fb30c3b6430
Alexandre Gouy <contact@agouy.com>
2020-06-23 12:01:31 +0200

    Update workspace.R

--- workspace.R
+++ workspace.R
@@ -10,7 +10,7 @@ ui <- shinyUI(fluidPage(
   
   sidebarPanel(
     sliderInput("plotWidth", "Plot width (px)", 200, 2000, 500),
-    sliderInput("plotHeight", "Plot width (px)", 200, 2000, 1200),
+    sliderInput("plotHeight", "Plot height (px)", 200, 2000, 700),
     sliderInput("rowFontSize", "Row labels font size", 5, 20, 7, step = 1),
     checkboxInput("logScale", "Log scale", value = FALSE),
     "NB: for negative values, the opposite of the log transformed absolute is computed"
